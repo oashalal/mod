65cf1cb2912e402b8ec52103db8c7129e481b841
oashalal <ssviridov829@gmail.com>
2024-01-04 20:42:54 +0300

    test

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -19,7 +19,7 @@ public class ExampleJavaMod extends Mod{
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
+                dialog.cont.add("dialog").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
